bafd2a0d3bc20c2bd873e9d25c5177fb1bde4f3e
CardenasDiana_48102 <dinaa132980@gmail.com>
2016-09-20 12:30:17 -0700

    Signed-off-by: CardenasDiana_48102 <dinaa132980@gmail.com>

---
+++ Homework/Assignment - 2/Gaddis_8thEd_Chap3_Prob22_SinCosTan/main.cpp
@@ -0,0 +1,123 @@
+/* 
+   File:   main
+   Author: Cardenas, Diana
+   Created on September 20, 2016, 12:14 PM
+   Purpose:  
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+#include <iomanip>
+#include <cmath>
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+const float PI=4*atan(1.0);  //PI
+const float CNVDEGR=PI/180;  //Conversion from degrees to radians
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    float deg, radians;
+    
+    //Input values
+    deg+=5;
+    radians=deg*CNVDEGR;
+    
+    //Display Output
+    cout<<"[ Degrees  Radians  Sine     Cosine   Tanget ]"<<endl;
+    cout<<fixed<<showpoint<<setprecision(5);
+    cout<<"["<<setw(8)<<deg<<","<<setw(8)<<radians
+            <<","<<setw(8)<<sin(radians)<<","<<setw(8)<<cos(radians)
+            <<","<<setw(8)<<tan(radians)<<"]"<<endl;
+    
+    
+    //Input values
+    deg+=5;
+    radians=deg*CNVDEGR;
+    
+    //Display Output
+    cout<<fixed<<showpoint<<setprecision(5);
+    cout<<"["<<setw(8)<<deg<<","<<setw(8)<<radians
+            <<","<<setw(8)<<sin(radians)<<","<<setw(8)<<cos(radians)
+            <<","<<setw(8)<<tan(radians)<<"]"<<endl;
+    
+    
+     //Input values
+    deg+=5;
+    radians=deg*CNVDEGR;
+    
+    //Display Output
+    cout<<fixed<<showpoint<<setprecision(5);
+    cout<<"["<<setw(8)<<deg<<","<<setw(8)<<radians
+            <<","<<setw(8)<<sin(radians)<<","<<setw(8)<<cos(radians)
+            <<","<<setw(8)<<tan(radians)<<"]"<<endl;
+    
+    
+    //Input values
+    deg+=5;
+    radians=deg*CNVDEGR;
+    
+    //Display Output
+    cout<<fixed<<showpoint<<setprecision(5);
+    cout<<"["<<setw(8)<<deg<<","<<setw(8)<<radians
+            <<","<<setw(8)<<sin(radians)<<","<<setw(8)<<cos(radians)
+            <<","<<setw(8)<<tan(radians)<<"]"<<endl;
+    
+    //Input values
+    deg+=5;
+    radians=deg*CNVDEGR;
+    
+    //Display Output
+    cout<<fixed<<showpoint<<setprecision(5);
+    cout<<"["<<setw(8)<<deg<<","<<setw(8)<<radians
+            <<","<<setw(8)<<sin(radians)<<","<<setw(8)<<cos(radians)
+            <<","<<setw(8)<<tan(radians)<<"]"<<endl;
+    
+    //Input values
+    deg+=5;
+    radians=deg*CNVDEGR;
+    
+    //Display Output
+    cout<<fixed<<showpoint<<setprecision(5);
+    cout<<"["<<setw(8)<<deg<<","<<setw(8)<<radians
+            <<","<<setw(8)<<sin(radians)<<","<<setw(8)<<cos(radians)
+            <<","<<setw(8)<<tan(radians)<<"]"<<endl;
+    
+    //Input values
+    deg+=5;
+    radians=deg*CNVDEGR;
+    
+    //Display Output
+    cout<<fixed<<showpoint<<setprecision(5);
+    cout<<"["<<setw(8)<<deg<<","<<setw(8)<<radians
+            <<","<<setw(8)<<sin(radians)<<","<<setw(8)<<cos(radians)
+            <<","<<setw(8)<<tan(radians)<<"]"<<endl;
+    
+    //Input values
+    deg+=5;
+    radians=deg*CNVDEGR;
+    
+    //Display Output
+    cout<<fixed<<showpoint<<setprecision(5);
+    cout<<"["<<setw(8)<<deg<<","<<setw(8)<<radians
+            <<","<<setw(8)<<sin(radians)<<","<<setw(8)<<cos(radians)
+            <<","<<setw(8)<<tan(radians)<<"]"<<endl;
+    
+    //Input values
+    deg+=5;
+    radians=deg*CNVDEGR;
+    
+    //Display Output
+    cout<<fixed<<showpoint<<setprecision(5);
+    cout<<"["<<setw(8)<<deg<<","<<setw(8)<<radians
+            <<","<<setw(8)<<sin(radians)<<","<<setw(8)<<cos(radians)
+            <<","<<setw(8)<<tan(radians)<<"]"<<endl;
+
+    //Exit Program
+    return 0;
+}
