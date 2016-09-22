a900d84f41c458c9c74c918197cd281c1c33b73f
diana2670753 <diana132980@gmail.com>
2016-09-22 12:41:40 -0700

    Signed-off-by: diana2670753 <diana132980@gmail.com>

---
+++ Homework/Assignment - 2/Gaddis_7thEd_Chap3_Prob1_MilesPerGallon/main.cpp
@@ -0,0 +1,31 @@
+/* 
+   File:   main
+   Author: Cardenas, Diana
+   Created on September 22, 2016, 12:39 PM
+   Purpose: Calculate the user's MPG on car 
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    
+    //Input values
+    
+    //Process values -> Map inputs to Outputs
+    
+    //Display Output
+
+    //Exit Program
+    return 0;
+}
+
